da6bacfbde9a7572b51cfa6e67e691b3c05b45b4
Adam <adagr407@student.liu.se>
2015-01-23 22:48:21 +0100

    Init commit

---
+++ AngelsAndDemons/Assets/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate() {
+		transform.Rotate(new Vector3(0,0,1)*Time.fixedDeltaTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
