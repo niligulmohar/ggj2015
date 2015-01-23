e5936e01f9946ac49e8bffd5b81f7a16f053fde8
Adam <adagr407@student.liu.se>
2015-01-23 23:32:10 +0100

    Merge conflict

---
+++ AngelsAndDemons/Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform myTarget;
+	public float lerpTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, myTarget.position, lerpTime);
+	}
+}
---
+++ AngelsAndDemons/Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private CharacterController myController;
+	public Transform myCamera;
+
+	public float turnSpeed;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		myController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(transform.up, Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed);
+
+		myController.SimpleMove(transform.forward * moveSpeed * Input.GetAxis("Vertical"));
+		
+	
+	}
+}
