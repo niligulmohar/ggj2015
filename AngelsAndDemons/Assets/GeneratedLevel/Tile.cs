28056ea017f0cb1d38e6f944e9a9df35aaef9baf
Nicklas Lindgren <nili@gulmohar.se>
2015-01-24 02:36:22 +0100

    Remove unused code

--- AngelsAndDemons/Assets/GeneratedLevel/Tile.cs
+++ AngelsAndDemons/Assets/GeneratedLevel/Tile.cs
@@ -4,8 +4,5 @@ using System.Collections;
 public class Tile : MonoBehaviour {
   public Transform[] randomSpawns;
   public void Init(int x, int y) {
-    foreach(var transform in randomSpawns) {
-      Debug.Log("TODO: Maybe spawn");
-    }
   }
 }
