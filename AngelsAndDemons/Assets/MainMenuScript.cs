4e5bcdbb44f59c4125d64cbc3012db7999e01470
Adam <adagr407@student.liu.se>
2015-01-25 13:41:00 +0100

    Add start button to main menu

---
+++ AngelsAndDemons/Assets/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public void NextLevel() {
+		Application.LoadLevel(1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
